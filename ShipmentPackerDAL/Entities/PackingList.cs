a104397a921affa24c4340ef0c4f16002542a838
Jeppe Ehmsen <jeppeehmsen@gmail.com>
2017-12-04 13:05:21 +0100

    DateTime change

--- ShipmentPackerDAL/Entities/PackingList.cs
+++ ShipmentPackerDAL/Entities/PackingList.cs
@@ -14,7 +14,7 @@ namespace ShipmentPackerDAL.Entities
 
         public string DeliveryAddress { get; set; }
 
-        public string DeliveryDate { get; set; }
+        public DateTime DeliveryDate { get; set; }
 
         public string ItemType { get; set; }
 
