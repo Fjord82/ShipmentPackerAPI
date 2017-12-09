963eea153fe540491bbc4b135f50e214d39a8cb7
Fjord82 <Fjordrasmus@gmail.com>
2017-12-09 11:22:55 +0100

    added testcases for create FreightConditions

---
+++ ShipmentPackerBLL/Services/FreightConditionService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using ShipmentPackerBLL.BusinessObjects;
+using ShipmentPackerBLL.Converters;
+using ShipmentPackerDAL;
+
+namespace ShipmentPackerBLL.Services
+{
+    public class FreightConditionService : IFreightConditionService
+    {
+        public IDALFacade _facade { get; set; }
+        FreightConditionConverter _conv;
+
+        public FreightConditionService(IDALFacade facade)
+        {
+            _facade = facade;
+            _conv = new FreightConditionConverter();
+        }
+
+        public FreightConditionBO Create(FreightConditionBO condition)
+        {
+            throw new NotImplementedException();
+        }
+
+        public FreightConditionBO Delete(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public FreightConditionBO Get(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<FreightConditionBO> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public FreightConditionBO Update(FreightConditionBO condition)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
